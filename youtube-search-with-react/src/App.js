fc9a763c41ded107060c09219a35589adf762baf
Saad Yousaf <saad.yousaf@arbisoft.com>
2017-09-21 14:42:09 +0500

    Setting up youtube api package and including API key.

--- youtube-search-with-react/src/App.js
+++ youtube-search-with-react/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import YTSearch from 'youtube-api-search';
 import './App.css';
+import { myConfig } from './config';
+
+const key = myConfig.API_KEY;
 
 class App extends Component {
   render() {
+   YTSearch({ key: key, term: 'hello' }, (data) => {
+        console.log(data)
+   });
+
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
